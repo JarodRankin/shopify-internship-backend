9bde02416be7ec4dbdf6083f9701fae480043fb1
Jarod Rankin <jarodr1187@yahoo.ca>
2022-01-16 15:46:17 -0500

    Add ability to update sku

--- inventory/config/routes.rb
+++ inventory/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
-  Rails.application.routes.draw do
-
-    get 'inventory/all', to: 'inventory#all'
-    get 'inventory/sku/:token', to: 'inventory#find_sku'
-    post 'inventory/sku', to: 'inventory#new_sku'
-
-  end
+  get 'inventory/all', to: 'inventory#all'
+  get 'inventory/sku/:token', to: 'inventory#find_sku'
+  post 'inventory/sku', to: 'inventory#new_sku'
+  patch 'inventory/sku', to: 'inventory#update_sku'
 
 end
