45d82c2897ecaa49ccacc226970d0c743fd85222
Chris Feher <fehercb@gmail.com>
2022-01-16 14:51:35 -0500

    Basic Sku model and inventory controller
    - Also converted app back to an API-Only app

---
+++ inventory/app/controllers/inventory_controller.rb
@@ -0,0 +1,58 @@
+require 'uuidtools'
+
+class InventoryController < ActionController::Base
+
+	# return a list of all sku's
+	# @return [Sku]
+	def all
+		skus = Sku.all
+		render json: skus
+	end
+
+	# returns a unique sku by token
+	# @parameter :token => String
+	# @return Sku
+	def find_sku
+		sku = sku_with_token(params[:token])
+
+		if sku && sku.valid?
+			render json: sku
+		else
+			head :no_content
+		end
+
+	end
+
+	# Creates a new sku and assigns it a unique token if one is not
+	# provided.
+	# @parameter :sku => JSON Representation of a Sku
+	# @return Sku
+	def new_sku
+		sku_json = params[:sku]
+
+		# Check if the sku parameter is provided
+		if !sku_json then head :bad_request end
+
+		# Pull the optional paramter token out of params
+		token = sku_json[:token]
+
+		# Check if token was provided, if not set to random UUID prefix
+		if !token then token = token = UUIDTools::UUID.timestamp_create.to_s[0..7] end
+		
+		# Try to find an existing Sku with the same token prior to creating
+		if sku_with_token(token) then head :bad_request end
+
+		sku = Sku.create(:token => token, :description => sku_json[:description], :price_cents => sku_json[:price_cents], :quantity => sku_json[:quantity])
+
+		render json: sku
+	end
+
+	private
+
+	# returns a unique sku by token
+	# @return Sku
+	def sku_with_token(token)
+		Sku.find_by(token: token)
+	end
+
+end
