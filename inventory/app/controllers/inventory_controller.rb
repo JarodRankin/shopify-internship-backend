3c7bd8274493eb6c10bd5becac642a74d0d695f0
Jarod Rankin <jarodr1187@yahoo.ca>
2022-01-17 10:55:28 -0500

    Add error handling

--- inventory/app/controllers/inventory_controller.rb
+++ inventory/app/controllers/inventory_controller.rb
@@ -1,6 +1,6 @@
 require 'uuidtools'
 
-class InventoryController < ActionController::Base
+class InventoryController < ApplicationController
 	
 	# return a list of all sku's
 	# @return [Sku]
@@ -30,30 +30,23 @@ class InventoryController < ActionController::Base
 	def new_sku
 		sku_json = params[:sku]
 
-		# Check if the sku parameter is provided
 		if !sku_json then
-			# TODO: - Render error
-			head :bad_request
-			return
+			render json: Error.new('Missing parameter \"sku\"', :bad_request), :status => :bad_request
+		else
+			token = sku_json[:token]
+
+			if !token then token = token = UUIDTools::UUID.timestamp_create.to_s[0..7] end
+
+			if sku_with_token(token) then
+				render json: Error.new('Sku already exists for provided token', :bad_request), :status => :bad_request
+			else
+				# TODO: - Figure out why I cant create a Sku with the hash in params
+				sku = Sku.create(:token => token, :description => sku_json[:description], :price_cents => sku_json[:price_cents], :quantity => sku_json[:quantity])
+				render json: sku
+			end
+
 		end
 
-		# Pull the optional paramter token out of params
-		token = sku_json[:token]
-
-		# Check if token was provided, if not set to random UUID prefix
-		if !token then token = token = UUIDTools::UUID.timestamp_create.to_s[0..7] end
-		
-		# Try to find an existing Sku with the same token prior to creating
-		if sku_with_token(token) then
-			# TODO: - Render error
-			head :bad_request
-			return
-		end
-
-		# TODO: - Figure out why I cant create a Sku with the hash in params
-		sku = Sku.create(:token => token, :description => sku_json[:description], :price_cents => sku_json[:price_cents], :quantity => sku_json[:quantity])
-
-		render json: sku
 	end
 
 	def update_sku
