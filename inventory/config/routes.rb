1b28fc23d108405d39ec48aec44b2abf6efaf8b9
Jarod Rankin <jrankin@Jarods-MacBook-Pro.local>
2022-01-17 21:56:13 -0500

    add delere method

--- inventory/config/routes.rb
+++ inventory/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'inventory/sku/:token', to: 'inventory#find_sku'
   post 'inventory/sku', to: 'inventory#new_sku'
   patch 'inventory/sku', to: 'inventory#update_sku'
+  delete 'inventory/sku', to: 'inventory#delete_sku'
 
 end
