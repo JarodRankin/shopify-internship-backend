8983a5de943ce927c1be22278438835e4500c7c8
Jarod Rankin <jarodr1187@yahoo.ca>
2022-01-18 10:26:59 -0500

    Cleanup code in Inventory Controller

--- inventory/config/routes.rb
+++ inventory/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   get 'inventory/all', to: 'inventory#all'
-  get 'inventory/sku/:token', to: 'inventory#find_sku'
+  get 'inventory/sku', to: 'inventory#find_sku'
   post 'inventory/sku', to: 'inventory#new_sku'
   patch 'inventory/sku', to: 'inventory#update_sku'
   delete 'inventory/sku', to: 'inventory#delete_sku'
   post 'inventory/delete', to: 'inventory#delete_sku'
   delete 'inventory/delete', to: 'inventory#undo_delete'
+
 end
