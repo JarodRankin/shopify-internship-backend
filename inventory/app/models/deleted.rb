c61f145afc587386af9f3b4c4c9774ff62a6e84d
Jarod Rankin <jrankin@Jarods-MacBook-Pro.local>
2022-01-18 19:12:51 -0500

    add deleted database

---
+++ inventory/app/models/deleted.rb
@@ -0,0 +1,11 @@
+class Deleted < ApplicationRecord
+	include ActiveModel::Serializers::JSON
+
+	validates :token, :description, :quantity, :price_cents, presence: true
+
+	def as_json(options={})
+		options[:except] ||= [:id]
+		super(options)
+	end
+
+end
